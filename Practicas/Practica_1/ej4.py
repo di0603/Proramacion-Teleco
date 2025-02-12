e64d77e5ab67d35d5e747ed87907dcf4ec2f3f0b
di0603 <irnpf06@gmail.com>
2025-02-12 12:21:54 +0100

    [ADD] Practica 1

---
+++ Practicas/Practica_1/ej4.py
@@ -0,0 +1,11 @@
+a = input("Introduce el valor de a: ")
+b = input("Introduce el valor de b: ")
+c = input("Introduce el valor de c: ")
+
+b = a
+a = c
+c = b
+
+print("El valor de a es: ", a)
+print("El valor de b es: ", b)
+print("El valor de c es: ", c)
