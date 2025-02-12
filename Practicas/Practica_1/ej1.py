a935a77474facdf01e82e89d4f25aeebe936892a
di0603 <irnpf06@gmail.com>
2025-02-12 11:23:12 +0100

    [ADD] Practica 1

---
+++ Practicas/Practica_1/ej1.py
@@ -0,0 +1,5 @@
+a = int(input("Introduce un numero: "))
+
+cuadrado = a * a
+
+print("El cuadrado es: ", cuadrado)
---
+++ Practicas/Practica_1/ej3.py
@@ -0,0 +1,9 @@
+a = input("Introduce el valor de la variable a: ")
+b = input("Introduce el valor de la variable b: ")
+
+c = b
+b = a
+a = c
+
+print("El valor de la variable a es: ", a)
+print("El valor de la variable b es: ", b)
